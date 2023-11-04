ac36bef1673c29bfaef050159dd7843ee2a2345c
HAE2NI <hae0_02ni@ewhain.net>
2023-11-04 09:48:24 +0900

    [ feat ] 숫자를 입력하지 않았을 경우 alert 뜨는 함수 구현

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
 const $ = (selector: string) =>
   document.getElementById(selector) as HTMLElement;
-const $$ = (selector: string) => document.querySelectorAll(selector);
 
 const firstNumber = $("first_number") as HTMLInputElement;
 const secondNumber = $("second_number") as HTMLInputElement;
 const resultBtn = $("result_button") as HTMLButtonElement;
 const selector = $("selector") as HTMLSelectElement;
 const resultHere = $("result") as HTMLHeadingElement;
+
+function blockString(inputDom: HTMLInputElement): void {
+  inputDom.addEventListener("input", (e) => {
+    const input = e.target as HTMLInputElement;
+    const changeToNum = input.value.replace(/[^0-9.]/g, "");
+    if (input.value != changeToNum) {
+      alert("숫자입력하세요");
+      input.value = "";
+    }
+  });
+}
+
+blockString(firstNumber);
+blockString(secondNumber);
 
 function calculator() {
   const num1 = parseFloat(firstNumber?.value);
@@ -26,7 +39,7 @@ function calculator() {
       result = num1 * num2;
       break;
     case "divide":
-      if (num2! == 0) {
+      if (num2 !== 0) {
         result = num1 / num2;
       } else {
         result = "계산 불가";
@@ -37,6 +50,8 @@ function calculator() {
       break;
   }
   resultHere.textContent = result.toString();
+  firstNumber.value = "";
+  secondNumber.value = "";
 }
 
 resultBtn.addEventListener("click", calculator);
